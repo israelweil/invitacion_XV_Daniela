111f428f308be833fd26f6ba63dd5739d48f4118
ISRAEL <l24120356@morelia.tecnm.mx>
2025-08-09 22:03:07 -0600

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx}", "./public/index.html"],
+  theme: { extend: {} },
+  plugins: []
+};
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // nuevo paquete para compatibilidad
+    autoprefixer: {},
+  },
+};
